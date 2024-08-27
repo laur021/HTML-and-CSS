5354942f1d772f40a56a2418b4dfdf8fd6717513
laur021 <30833750+laur021@users.noreply.github.com>
2024-08-27 08:39:14 +0800

    Add files via upload

---
+++ React/293-keeper-app-part-1-challenge/src/components/note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>This is the title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam consectetur facere, debitis ratione ullam voluptatem assumenda numquam, sunt voluptatibus suscipit consequuntur ipsum, ex fugiat a odio quis obcaecati dolor dolorum nam consequatur dicta. Voluptatem dolores reiciendis accusantium exercitationem labore, debitis laudantium sed hic quisquam ipsam neque vel! Aperiam, deleniti.</p>
+    </div>
+}
+
+export default Note;
